46d833020b9ab96fa79734864b540f7ca58f6649
Leifan Wang <leifanwang@appannie.com>
2018-12-17 11:12:41 +0800

    symmetric tree

---
+++ leetcode/no101_symmetric_tree.py
@@ -0,0 +1,21 @@
+class TreeNode(object):
+
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+
+    def is_symmetric(self, root):
+        if not root:
+            return True
+        return self.mirror(root.left, root.right)
+
+    def mirror(self, left, right):
+        if not left or right:
+            return left == right
+        if left.val != right.val:
+            return False
+        return self.mirror(left.left, right.right) and self.mirror(left.right, right.left)
